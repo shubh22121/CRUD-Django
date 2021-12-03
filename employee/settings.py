e48f89f9fe3d51379cf0cca6aecd65bc4f9e5cc3
shubham <shubham22121@gmail.com>
2021-12-04 03:46:50 +0530

    forms.py created

--- employee/settings.py
+++ employee/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'employee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Employee',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+
     }
 }
 
--- register/models.py
+++ register/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class position (models.Model):
+    title = models.CharField(max_length=50)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    emp_code = models.CharField(max_length=3)
+    phone = models.CharField(max_length=15)
+    position = models.ForeignKey(position, on_delete=models.CASCADE)
+    
